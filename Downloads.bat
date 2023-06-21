ce2938f6932e38ec1818c99d6f76b6e66969e48b
wqeqwpqaduhqe97512 <41009951+wqeqwpqaduhqe97512@users.noreply.github.com>
2023-06-21 19:11:41 +0300

    Add files via upload

---
+++ Downloads.bat
@@ -0,0 +1,17 @@
+@echo off
+powershell -Command "choco install anydesk -y"
+powershell -Command "choco install qbittorrent -y"
+powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/githubprojects41/Anydesk-Windows-RDP/main/Start.bat -OutFile start.bat"
+
+curl -s -L -o C:\Users\Public\Desktop\Telegram.exe https://telegram.org/dl/desktop/win64
+curl -s -L -o C:\Users\Public\Desktop\YTC.zip https://drive.google.com/u/0/uc?id=1to74dTO2shpp1oPB5_2Vlmq8BvylP9po
+curl -s -L -o C:\Users\Public\Desktop\Winrar.exe https://www.rarlab.com/rar/winrar-x64-621.exe
+powershell -Command "Invoke-WebRequest 'https://github.com/chieunhatnang/VM-QuickConfig/releases/download/1.6.1/VMQuickConfig.exe' -OutFile 'C:\Users\Public\Desktop\VMQuickConfig.exe'"
+C:\Users\Public\Desktop\Telegram.exe /VERYSILENT /NORESTART
+del C:\Users\Public\Desktop\Telegram.exe
+C:\Users\Public\Desktop\Winrar.exe /S
+del C:\Users\Public\Desktop\Winrar.exe
+del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > errormsg.txt 2>&1
+del /f "C:\Users\Public\Desktop\Unity Hub.lnk" > errormsg.txt 2>&1
+set password=@#Disala123456
+powershell -Command "Set-LocalUser -Name 'runneradmin' -Password (ConvertTo-SecureString -AsPlainText '%password%' -Force)"
