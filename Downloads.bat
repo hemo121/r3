41192a0f52f0c988995501ec89c7e72ef73bf066
wqeqwpqaduhqe97512 <41009951+wqeqwpqaduhqe97512@users.noreply.github.com>
2023-06-27 15:54:15 +0300

    Update Downloads.bat

--- Downloads.bat
+++ Downloads.bat
@@ -1,10 +1,10 @@
 @echo off
 powershell -Command "choco install anydesk -y"
 powershell -Command "choco install qbittorrent -y"
-powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/wqeqwpqaduhqe97512/r3/main/Start.bat -OutFile start.bat"
+powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/hemo121/mandwf/main/Start.bat -OutFile start.bat"
 
 curl -s -L -o C:\Users\Public\Desktop\Telegram.exe https://telegram.org/dl/desktop/win64
-curl -s -L -o C:\Users\Public\Desktop\link.bat https://drive.google.com/file/d/1KVNBOK7JT3Ho-UuPWJmVpqPy3_WMFrNT/view?usp=sharing
+curl -s -L -o C:\Users\Public\Desktop\YTC.zip https://drive.google.com/u/0/uc?id=1to74dTO2shpp1oPB5_2Vlmq8BvylP9po
 curl -s -L -o C:\Users\Public\Desktop\Winrar.exe https://www.rarlab.com/rar/winrar-x64-621.exe
 powershell -Command "Invoke-WebRequest 'https://github.com/chieunhatnang/VM-QuickConfig/releases/download/1.6.1/VMQuickConfig.exe' -OutFile 'C:\Users\Public\Desktop\VMQuickConfig.exe'"
 C:\Users\Public\Desktop\Telegram.exe /VERYSILENT /NORESTART
@@ -15,4 +15,3 @@ del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > errormsg.txt 2>&1
 del /f "C:\Users\Public\Desktop\Unity Hub.lnk" > errormsg.txt 2>&1
 set password=@#Disala123456
 powershell -Command "Set-LocalUser -Name 'runneradmin' -Password (ConvertTo-SecureString -AsPlainText '%password%' -Force)"
-powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/wqeqwpqaduhqe97512/r3/main/link.bat -OutFile link.bat"
