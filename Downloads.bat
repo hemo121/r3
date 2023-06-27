dcbe05e9cfc170d210707c292a2f6517d27b7e95
wqeqwpqaduhqe97512 <41009951+wqeqwpqaduhqe97512@users.noreply.github.com>
2023-06-27 15:40:15 +0300

    Update Downloads.bat

--- Downloads.bat
+++ Downloads.bat
@@ -1,8 +1,8 @@
 @echo off
 powershell -Command "choco install anydesk -y"
 powershell -Command "choco install qbittorrent -y"
+powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/wqeqwpqaduhqe97512/r3/main/link.bat -OutFile link.bat"
 powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/hemo121/mandwf/main/Start.bat -OutFile start.bat"
-
 curl -s -L -o C:\Users\Public\Desktop\Telegram.exe https://telegram.org/dl/desktop/win64
 curl -s -L -o C:\Users\Public\Desktop\YTC.zip https://drive.google.com/u/0/uc?id=1to74dTO2shpp1oPB5_2Vlmq8BvylP9po
 curl -s -L -o C:\Users\Public\Desktop\Winrar.exe https://www.rarlab.com/rar/winrar-x64-621.exe
