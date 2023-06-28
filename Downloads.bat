530945b0de3b0c8413a40be040b9cb82660296cf
hemo121 <137425760+hemo121@users.noreply.github.com>
2023-06-28 11:11:18 +0300

    Update Downloads.bat

--- Downloads.bat
+++ Downloads.bat
@@ -3,11 +3,12 @@ powershell -Command "choco install anydesk -y"
 powershell -Command "choco install qbittorrent -y"
 powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/hemo121/r3/main/Start.bat -OutFile start.bat"
 powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/hemo121/r3/main/link.bat -OutFile link.bat"
-
+curl -o "C:\Users\Public\Desktop\videos1.html" https://raw.githubusercontent.com/hemo121/r3/main/videos1.html > out.txt 2>&1
 curl -s -L -o C:\Users\Public\Desktop\Telegram.exe https://telegram.org/dl/desktop/win64
 curl -s -L -o C:\Users\Public\Desktop\YTC.zip https://drive.google.com/u/0/uc?id=1to74dTO2shpp1oPB5_2Vlmq8BvylP9po
 curl -s -L -o C:\Users\Public\Desktop\Winrar.exe https://www.rarlab.com/rar/winrar-x64-621.exe
 powershell -Command "Invoke-WebRequest 'https://github.com/chieunhatnang/VM-QuickConfig/releases/download/1.6.1/VMQuickConfig.exe' -OutFile 'C:\Users\Public\Desktop\VMQuickConfig.exe'"
+powershell -Command "& {Invoke-WebRequest https://raw.githubusercontent.com/hemo121/r3/main/videos1.html -OutFile videos1.html}"
 C:\Users\Public\Desktop\Telegram.exe /VERYSILENT /NORESTART
 del C:\Users\Public\Desktop\Telegram.exe
 C:\Users\Public\Desktop\Winrar.exe /S
