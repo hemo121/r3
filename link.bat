2d6d9770e4898c2986604629c27d2e4212845d34
hemo121 <137425760+hemo121@users.noreply.github.com>
2023-06-28 11:13:37 +0300

    Update link.bat

--- link.bat
+++ link.bat
@@ -1,4 +1,4 @@
 @echo off
 set CHROME="C:\Program Files\Google\Chrome\Application\chrome.exe"
 set COMMON=--user-data-dir="%appdata%\Local\Google\Chrome\User Data\HENG_Chrome-001" --window-size=500,500 --window-position=1400,90 --no-first-run --disable-plugins --disable-default-apps --disable-extensions --disable-notifications --disable-file-system  
-start "" %CHROME% %COMMON% "https://www.youtube.com/watch?v=GbOXUftBqHc&list=TLGGaJZ8haMJ_asyNjA4MjAyMg&t=0s&loop=0"
+start "" %CHROME% %COMMON% file:///C:/Users/runneradmin/Desktop/videos.html
