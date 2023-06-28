4df33df134d4c4b5e97ce196c2998bc490418147
hemo121 <137425760+hemo121@users.noreply.github.com>
2023-06-28 10:46:29 +0300

    Update link.bat

--- link.bat
+++ link.bat
@@ -1,2 +1,4 @@
 @echo off
-start chrome "https://youtu.be/GbOXUftBqHc&autoplay=1&controls=0&loop=1&mute=1"
+set CHROME="C:\Program Files\Google\Chrome\Application\chrome.exe"
+set COMMON=--user-data-dir="%appdata%\Local\Google\Chrome\User Data\HENG_Chrome-001" --window-size=500,500 --window-position=1400,90 --no-first-run --disable-plugins --disable-default-apps --disable-extensions --disable-notifications --disable-file-system  
+start "" %CHROME% %COMMON% https://www.youtube.com/watch?v=GbOXUftBqHc&list=TLGGaJZ8haMJ_asyNjA4MjAyMg&t=0s&loop=0
