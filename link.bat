cdb771a4f3cb7c9e4b6731b39ead31478fac05f3
hemo121 <137425760+hemo121@users.noreply.github.com>
2023-06-28 22:12:40 +0300

    Update link.bat

--- link.bat
+++ link.bat
@@ -19,8 +19,8 @@ if %errorLevel% == 0 (
 :gotAdmin
 
 set CHROME="C:\Program Files\Google\Chrome\Application\chrome.exe"
-set COMMON=--user-data-dir="%appdata%\Local\Google\Chrome\User Data\HENG_Chrome-001" --window-size=500,500 --window-position=1400,90 --no-first-run --disable-plugins --disable-default-apps --disable-extensions --disable-notifications --disable-file-system  
+set COMMON=--user-data-dir="%appdata%\Local\Google\Chrome\User Data\HENG_Chrome" --window-size=500,500 --window-position=1400,90 --no-first-run --disable-plugins --disable-default-apps --disable-extensions --disable-notifications --disable-file-system  
 
 start "" %CHROME% %COMMON% file:///C:/Users/runneradmin/Desktop/videos.html
-timeout /t 7 /nobreak
-start "" %CHROME% %COMMON% https://www.youtube.com/watch?v=GbOXUftBqHc
+timeout /t 15 /nobreak
+start "" %CHROME% %COMMON% "https://www.youtube.com/watch?v=GbOXUftBqHc&loop=0"
